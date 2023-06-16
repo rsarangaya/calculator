f60e29ae8027bb32efae3322360dbb689e9248ee
rsarangaya <103656755+rsarangaya@users.noreply.github.com>
2023-06-16 09:25:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function insert(num){
+    document.form.textinput.value = document.form.textinput.value + num;
+  }
+function clean() {
+    
+    document.form.textinput.value = "";
+  }
+function back() {
+    var text = document.form.textinput.value   
+   // remove last character from string
+    document.form.textinput.value = text.substring(0,text.length-1)  }
+function calculate(){
+    // eval calculation
+    var text = document.form.textinput.value
+    document.form.textinput.value = eval(text)  
+}
